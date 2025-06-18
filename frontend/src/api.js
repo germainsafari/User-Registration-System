1a0f19a3e7b15dacd7ba3eb6cd60d327935b7200
Safari Germain <pzx115866@student.wsb.poznan.pl>
2025-06-18 13:37:15 +0200

    docker

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const API_URL = process.env.REACT_APP_API_URL || "https://user-registration-system-13we.onrender.com";
 
 export const registerUser = (data) =>
   axios.post(`${API_URL}/register`, data);
